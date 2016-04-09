68093a1d7d3e6474016e24f0db74764123ad4b4f
bliberini <bliberini@gmail.com>
2016-04-09 17:36:54 -0300

    Travis

---
+++ test/testTestable.js
@@ -0,0 +1,25 @@
+/**
+ * Created by bliberini on 4/9/16.
+ */
+var chai = require('chai');
+var expect = chai.expect;
+var CartSummary = require('./../testable.js');
+
+describe('Testable', function () {
+    it('getSubTotal() should return 0', function () {
+        var car = new CartSummary([{
+            id: 1,
+            quantity: 4,
+            price: 50
+        }, {
+            id: 2,
+            quantity: 2,
+            price: 30
+        }, {
+            id: 3,
+            quantity: 1,
+            price: 40
+        }]);
+        expect(car.getSubtotal()).to.equal(300);
+    })
+})
