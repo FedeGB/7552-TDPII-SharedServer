d373934e0dbd7207d831eb02c2d47925fadb90a5
bliberini <bliberini@gmail.com>
2016-04-23 00:07:43 -0300

    front end listado intereses: close #6

--- public/js/ctrls/interestsController.js
+++ public/js/ctrls/interestsController.js
@@ -4,7 +4,21 @@
 (function () {
     'use strict';
     
-    app.controller("interestsController", function ($scope) {
-       var self = this; 
+    app.controller("interestsController", function ($scope, dbService) {
+        var self = this;
+        self.interests = [];
+        self.categories = [];
+        self.load = function () {
+            dbService.getInterests().then(function (data) {
+                self.interests = data.data.interests;
+                self.interests.forEach(function (interest) {
+                    if (self.categories.indexOf(interest.category) < 0) {
+                        self.categories.push(interest.category);
+                    }
+                });
+            });
+        };
+
+        self.load();
     });
 })();
